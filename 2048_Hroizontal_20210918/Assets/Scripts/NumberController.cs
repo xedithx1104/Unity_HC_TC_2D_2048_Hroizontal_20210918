bfd8c6b0b40fa900edb8ef8695c8f6c9b211a743
KID <50666207+KID421@users.noreply.github.com>
2021-11-21 08:49:07 +0800

    Update 2048 生成與向右移動

--- 2048_Hroizontal_20210918/Assets/Scripts/NumberController.cs
+++ 2048_Hroizontal_20210918/Assets/Scripts/NumberController.cs
@@ -3,17 +3,19 @@ using UnityEngine;
 public class NumberController : MonoBehaviour
 {
     public NumberManager numberManager;
+    public Direction direction;
 
     private bool startClick;
     private Vector2 positionStart;
     private Vector2 positionEnd;
+    private Vector2 v2Direction;
 
     private void Update()
     {
-        Direction();
+        ClickCheck();
     }
 
-    private void Direction()
+    private void ClickCheck()
     {
         if (!startClick && Input.GetKeyDown(KeyCode.Mouse0))
         {
@@ -23,13 +25,28 @@ public class NumberController : MonoBehaviour
         else if (startClick && Input.GetKeyUp(KeyCode.Mouse0))
         {
             positionEnd = Input.mousePosition;
-            print(positionEnd - positionStart);
+            v2Direction = positionEnd - positionStart;
 
-            Vector2 direction = positionEnd - positionStart;
-            print(direction.normalized.x);
-            print(direction.normalized.y);
+            direction = DirectionCheck();
+
+            numberManager.MoveBlock(direction);
 
             startClick = false;
         }
     }
+
+    private Direction DirectionCheck()
+    {
+        Direction result = Direction.none;
+
+        if (Mathf.Abs(v2Direction.x) > Mathf.Abs(v2Direction.y)) result = v2Direction.x > 0 ? Direction.right : Direction.left;
+        else if (Mathf.Abs(v2Direction.x) < Mathf.Abs(v2Direction.y)) result = v2Direction.y > 0 ? Direction.up : Direction.down;
+
+        return result;
+    }
+}
+
+public enum Direction
+{
+    none, right, left, up, down
 }
--- 2048_Hroizontal_20210918/Assets/Scripts/NumberManager.cs
+++ 2048_Hroizontal_20210918/Assets/Scripts/NumberManager.cs
@@ -17,14 +17,33 @@ public class NumberManager : MonoBehaviour
             for (int j = 0; j < blocks.GetLength(1); j++)
             {
                 blocks[i, j] = new BlockData();
-                blocks[i, j].index = new Vector2(i, j);
+                blocks[i, j].index = new Vector2Int(i, j);
                 blocks[i, j].position = emptyBlocks[i * 4 + j].position;
-
-                //print("°}¦C®y¼Ð¡G" + i + ", " + j + " | " + "°}¦C®y¼Ð¡G" + blocks[i, j].position);
             }
         }
 
+        PrintAllBlock();
+
         CreateNumber();
+        CreateNumber();
+        PrintAllBlock();
+    }
+
+    private void PrintAllBlock()
+    {
+        string result = "";
+
+        for (int i = 0; i < blocks.GetLength(0); i++)
+        {
+            for (int j = 0; j < blocks.GetLength(1); j++)
+            {
+                result += "( " + i + ", " + j + " ) - <color=red>" + blocks[i, j].number + "</color> | ";
+            }
+
+            result += "\n";
+        }
+
+        print(result);
     }
 
     private void CreateNumber()
@@ -33,23 +52,67 @@ public class NumberManager : MonoBehaviour
                     where i.number == 0
                     select i;
 
-        foreach (var item in query)
-        {
-            //print(item.position);
-        }
-
         int r = Random.Range(0, query.Count());
         BlockData data = query.ToArray()[r];
 
-        print("ÀH¾÷¡G" + data.position);
         RectTransform tempBlock = Instantiate(blockWithNuumber, canvasBlock).GetComponent<RectTransform>();
         tempBlock.position = data.position;
+        blocks[data.index.x, data.index.y].number = 2;
+        blocks[data.index.x, data.index.y].targetBlock = tempBlock;
+    }
+
+    public void MoveBlock(Direction direction)
+    {
+        switch (direction)
+        {
+            case Direction.none:
+                break;
+            case Direction.right:
+                MoveBlockAndCheck();
+                break;
+            case Direction.left:
+                break;
+            case Direction.up:
+                break;
+            case Direction.down:
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void MoveBlockAndCheck()
+    {
+        for (int i = 0; i < blocks.GetLength(0); i++)
+        {
+            for (int j = blocks.GetLength(1) - 1; j >= 0; j--)
+            {
+                if (blocks[i, j].number == 0) continue;
+                if (j == blocks.GetLength(1) - 1) continue;
+
+                for (int k = blocks.GetLength(1) - 1; k != j; k--)
+                {
+                    if (blocks[i, k].number == 0)
+                    {
+                        blocks[i, k].targetBlock = blocks[i, j].targetBlock;
+                        blocks[i, j].targetBlock = null;
+                        blocks[i, k].number = blocks[i, j].number;
+                        blocks[i, j].number = 0;
+                        blocks[i, k].targetBlock.position = blocks[i, k].position;
+                        break;
+                    }
+                }
+            }
+        }
+
+        PrintAllBlock();
     }
 }
 
 public class BlockData
 {
-    public Vector2 index;
+    public Vector2Int index;
     public int number;
     public Vector2 position;
+    public RectTransform targetBlock;
 }
